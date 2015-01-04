251a986ed3ef904a448d12443bf4332cf96a00cb
litianzhao <litianzhao@tangpool.com>
2015-01-04 10:15:48 +0800

    更新了依赖库到最新版本；修改了默认配置

--- index.js
+++ index.js
@@ -8,21 +8,16 @@
 var minify = require('html-minifier').minify;
 
 module.exports = function(content, file, conf){
-    return minify(content, conf);
+    if (file.extras.minifyHTML) {
+        var options = fis.util.merge(module.exports.defaultOptions, conf);
+        return minify(content, options);
+    }
 };
 
 module.exports.defaultOptions = {
     removeComments:                true,
-    removeCommentsFromCDATA:       true,
-//    removeCDATASectionsFromCDATA:  true,
     collapseWhitespace:            true,
-    collapseBooleanAttributes:     true,
-    removeAttributeQuotes:         true,
-    removeRedundantAttributes:     true,
     useShortDoctype:               true,
-    removeEmptyAttributes:         true,
-//    removeEmptyElements:           true,
-//    removeOptionalTags:            true,
     removeScriptTypeAttributes:    true,
     removeStyleLinkTypeAttributes: true
 };
